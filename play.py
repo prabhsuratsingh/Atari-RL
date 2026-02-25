e3c334b2283e60ff695a186253050850ffd60cb0
prabhsuratsingh <prabhsurat17@gmail.com>
2026-02-25 22:24:14 +0530

    run dqn on pong

---
+++ play.py
@@ -0,0 +1,49 @@
+import gymnasium as gym 
+import argparse
+import numpy as np
+import typing as tt
+from sympy import true
+import torch
+import dqn
+import utils
+import collections
+import ale_py
+
+gym.register_envs(ale_py)
+
+ENV_NAME = "PongNoFrameskip-v4"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", required=True, help="Model file to load")
+    parser.add_argument("-e", "--env", default=ENV_NAME, help="Name of environment")
+    parser.add_argument("-r", "--record", required=True, help="Directory for video recording")
+    args = parser.parse_args()
+
+    env = utils.make_env(args.env, render_mode="rgb_array")
+    env = gym.wrappers.RecordVideo(env, video_folder=args.record)
+    net = dqn.DQN(env.observation_space.shape, env.action_space.n)
+    state = torch.load(args.model, map_location=lambda stg, _: stg)
+    net.load_state_dict(state)
+
+    state, _ = env.reset()
+    total_reward = 0.0
+    c: tt.Dict[int,int] = collections.Counter()
+
+    while True:
+        state_v = torch.as_tensor(state).unsqueeze(0)
+        q_vals = net(state_v).detach().cpu().numpy()[0]
+        action = int(np.argmax(q_vals))
+        c[action] += 1
+
+        state, reward, is_done, is_trunc, _ = env.step(action)
+        total_reward += reward
+
+        if is_done or is_trunc:
+            break
+    
+    print(f"Total reward: {total_reward:.2f}")
+    print(f"Action counts: {c}")
+
+    env.close()
